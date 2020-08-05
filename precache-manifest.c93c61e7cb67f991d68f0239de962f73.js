fb05758646df9b24da1c00cfa090962dac8c7a59
DominikHor <dhorstkamp@aol.com>
2020-08-05 14:57:21 +0200

    Updates

---
+++ precache-manifest.c93c61e7cb67f991d68f0239de962f73.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0db385e7b77fbcd1224aebccceb79dfc",
+    "url": "/dndworld/index.html"
+  },
+  {
+    "revision": "61f9f67970f53ce27e35",
+    "url": "/dndworld/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "1bf21253fc330cdd0564",
+    "url": "/dndworld/static/js/2.1fc6356a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/dndworld/static/js/2.1fc6356a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "61f9f67970f53ce27e35",
+    "url": "/dndworld/static/js/main.5263d5b0.chunk.js"
+  },
+  {
+    "revision": "1868434a8dd116eefc95",
+    "url": "/dndworld/static/js/runtime-main.45d08be2.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/dndworld/static/media/logo.5d5d9eef.svg"
+  }
+]);
